c19e7be063950abaca8889f2f7e6a063d0d04706
jonybang <Jonybange@gmail.com>
2019-01-28 03:18:16 +0200

    fixes

--- contracts/GaltToken.sol
+++ contracts/GaltToken.sol
@@ -15,7 +15,6 @@ pragma solidity ^0.5.0;
 
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
 
-
 contract GaltToken is ERC20Mintable {
   // solium-disable-next-line uppercase
   string public constant _name = "Galt Token";
